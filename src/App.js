469aaa9d6fb735a732a975e75311cb4ba4398a7f
sahil <sahil.gohil@yahoo.in>
2023-02-21 01:22:52 +1100

    feature no-go-top on mobile device

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { Routes,Route} from "react-router-dom";
 import Home from "./Home";
 import PostPage from './components/PostPage'
 import About from "./components/About";
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 import { useLocation } from "react-router-dom";
+import React from "react";
+
 
 
 function App() {
@@ -16,15 +18,33 @@ function App() {
   //newsapi.org
 // api key= 7b5391f0b30c443786924b2a5038dff6
 
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [isWide,setIsWide] = useState(false)
+  useEffect(() => {
+    function handleResize() {
+      setWindowWidth(window.innerWidth);
+      if(windowWidth > 632)
+      {
+        setIsWide(true)
+      }
+      else if(windowWidth <632){
+        setIsWide(false)
+      }
+    }
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [windowWidth]);
+
   function goTop()
   {
     window.scrollTo(10,10)
   }
+  console.log(window.innerWidth)
 
 
   return (
     <div className="App" >
-      <div className="go-top" onClick={goTop}><i class="fa-solid fa-arrow-up fa-xl"></i></div>
+      {isWide && <div className="go-top" onClick={goTop}><i class="fa-solid fa-arrow-up fa-xl"></i></div>}
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path=':id' element ={<PostPage />}/>
